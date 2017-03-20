4b44e0f3b158095792e7ec4e21d97033d66e7b27
LadyViktoria <leonieneon@gmail.com>
2017-03-20 01:09:09 +0100

    make script execution simple

--- buildAndroidAPS.bat
+++ buildAndroidAPS.bat
@@ -1,4 +1,4 @@
 Powershell -ExecutionPolicy Bypass -Command "ls -Recurse .\scripts\*.ps1 | Unblock-File"
-Powershell -ExecutionPolicy Bypass -Command Start-Process "$PSHome\PowerShell.exe" -ArgumentList ' -ExecutionPolicy Bypass -file ".\scripts\buildAndroidAPS.ps1"'
+Powershell -ExecutionPolicy Bypass -file ".\scripts\buildAndroidAPS.ps1"
 
 
