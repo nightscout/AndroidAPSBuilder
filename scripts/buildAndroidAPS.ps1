03e28dda1ae196dce47ab4e37d92605cb6fab313
LadyViktoria <leonieneon@gmail.com>
2017-03-25 15:19:19 +0100

    move sign

--- scripts/buildAndroidAPS.ps1
+++ scripts/buildAndroidAPS.ps1
@@ -93,7 +93,7 @@ $options = "First install Powershell 5 only for win 7/8/8.1","Install Git","Inst
 		"Switch to or update dev Branch`r`n" {cls;fetchMainRepo;resetRepo dev;anykey;MainMenu}
 		"Build" {buildaaps}
 		"Generate key for signing" {cls;generateKey;anykey;MainMenu}
-		"Sign APK's" {cls;.$scriptroot\signAPK.ps1;anykey;MainMenu}
+		"Sign APK's" {cls;signAPK;anykey;MainMenu}
 		"Install APK`r`n" {cls;.$scriptroot\ADB.ps1;anykey;MainMenu}
 		"-Exit-" {Exit}
 	}
@@ -187,6 +187,67 @@ Get-ChildItem $apkFolder -Filter *debug.apk | Foreach-Object {
 		write-host "======================================================"
 		Copy-Item "$fullname" -Destination (New-Item "$parentFolder\apk\" -Type container -Force) -Force
 		}
+}
+
+function copyApk {
+Get-ChildItem $apkFolder -Filter *.apk | Foreach-Object {
+		$fullname = $_.FullName
+		write-host "======================================================" 
+		write-host "copy $_ to`r`n$parentFolder\apk\" -foregroundcolor yellow
+		write-host "======================================================`r`n"
+		Copy-Item "$fullname" -Destination (New-Item "$parentFolder\apk\" -Type container -Force) -Force
+		}
+}
+
+function signAPK {
+$buildTools = (gci $androidSDK\build-tools\ | sort LastWriteTime | select -last 1).FullName
+$keystorepw = read-host "Keystore password"
+copyApk
+Get-ChildItem $parentFolder\apk -Filter *unsigned.apk | 
+	Foreach-Object {
+		write-host "======================================================"
+		write-host "Signing $_" -foregroundcolor magenta
+		write-host "======================================================"
+		write-host ""
+		$basename = $_.BaseName
+		$signedName = $basename.Replace("unsigned","signed")
+		$zipalign = cmd /c $buildtools\zipalign.exe -p 4 $_.FullName $parentFolder\apk\$basename-aligned.apk '2>&1' | Out-String | Tee-Object -Variable zipalign
+		$signer= cmd /c $buildtools\apksigner.bat sign --verbose --ks $parentFolder\aaps-release-key.jks --ks-pass pass:$keystorepw --out $parentFolder\apk\$signedName.apk $parentFolder\apk\$basename-aligned.apk '2>&1' | Out-String | Tee-Object -Variable signer
+		if ($signer -like "*password was incorrect*") {
+		write-host "password was incorrect" -foregroundcolor red
+		anykey
+		MainMenu
+		} else {
+		$verify = cmd /c $buildtools\apksigner.bat verify -v $parentFolder\apk\$signedName.apk '2>&1' | Out-String | Tee-Object -Variable verify
+		write-host -nonewline $verify
+		write-host "Signing of $signedName.apk complete"  -foregroundcolor magenta
+		write-host ""}
+	}
+
+Get-ChildItem $parentFolder\apk\ -Filter *debug.apk | 
+	Foreach-Object {
+		write-host "======================================================"
+		write-host "Signing $_" -foregroundcolor magenta
+		write-host "======================================================"
+		write-host ""
+		$basename = $_.BaseName
+		$signedName = $basename.Replace("debug","debug-release-signed")
+		$zipalign = cmd /c $buildtools\zipalign.exe -p 4 $_.FullName $parentFolder\apk\$basename-aligned.apk '2>&1' | Out-String | Tee-Object -Variable zipalign
+		$signer= cmd /c $buildtools\apksigner.bat sign --verbose --ks $parentFolder\aaps-release-key.jks --ks-pass pass:$keystorepw --out $parentFolder\apk\$signedName.apk $parentFolder\apk\$basename-aligned.apk '2>&1' | Out-String | Tee-Object -Variable signer
+		if ($signer -like "*password was incorrect*") {
+		write-host "password was incorrect" -foregroundcolor red
+		anykey
+		MainMenu
+		} else {
+		$verify = cmd /c $buildtools\apksigner.bat verify -v $parentFolder\apk\$signedName.apk '2>&1' | Out-String | Tee-Object -Variable verify
+		write-host -nonewline $verify
+		write-host "Signing of $signedName.apk complete"  -foregroundcolor magenta
+		write-host ""}
+	}
+	
+Get-ChildItem $parentFolder\apk\ -Filter *debug.apk | Remove-Item
+Get-ChildItem $parentFolder\apk\ -Filter *aligned.apk | Remove-Item
+Get-ChildItem $parentFolder\apk\ -Filter *unsigned.apk | Remove-Item
 }
 
 function Set-Key {
