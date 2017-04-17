9b258df4d8a1d0e0e5074c0e1f74d92e1ef6cdf3
LadyViktoria <leonieneon@gmail.com>
2017-04-17 14:17:33 +0200

    small fix

--- scripts/buildAndroidAPS.ps1
+++ scripts/buildAndroidAPS.ps1
@@ -121,18 +121,6 @@ function buildaaps {
 checkAndroid_Home
 checkJava_Home
 checkaapsFolder
-$key = Set-Key "AndroidAPSpasswordkey"
-#$plainText = "password"
-#$encryptedTextThatIcouldSaveToFile = Set-EncryptedData -key $key -plainText $plaintext
-#$encryptedTextThatIcouldSaveToFile
-$passwordhash = "76492d1116743f0423413b16050a5345MgB8ADMAOQBIAG0AYwBkAEUANABTAGwARQBnAFcAVQBjAFAATwBoAGIASgB4AEEAPQA9AHwANwBlADQAYQAwADYAMwAxADUAMwBjADYAZgA1ADMAMQBlAGUAZQA3AGYAMwAxADMANQAwAGEAYQA4ADIAMQBiAA=="
-$password2 = Get-EncryptedData -data $passwordhash -key $key
-$password = read-host "password"
-if (!($password -eq $password2)) {
-	write-host "`r`nwrong password!" -foregroundcolor red
-	anykey
-	MainMenu
-	}
 $options = "Full","NSClient","Openloop","Pumpcontrol","-Main Menu-","-Exit-"
 	$selection = Menu $options "Select Build Flavor"
 	Switch ($selection) {
@@ -382,31 +370,6 @@ Get-ChildItem $parentFolder\apk\* -Include *unsigned.apk, *debug.apk |
 		}
 	}
 	removeApk
-}
-
-function Set-Key {
-param([string]$string)
-$length = $string.length
-$pad = 32-$length
-if (($length -lt 16) -or ($length -gt 32)) {Throw "String must be between 16 and 32 characters"}
-$encoding = New-Object System.Text.ASCIIEncoding
-$bytes = $encoding.GetBytes($string + "0" * $pad)
-return $bytes
-}
-
-function Set-EncryptedData {
-param($key,[string]$plainText)
-$securestring = new-object System.Security.SecureString
-$chars = $plainText.toCharArray()
-foreach ($char in $chars) {$secureString.AppendChar($char)}
-$encryptedData = ConvertFrom-SecureString -SecureString $secureString -Key $key
-return $encryptedData
-}
-
-function Get-EncryptedData {
-param($key,$data)
-$data | ConvertTo-SecureString -key $key |
-ForEach-Object {[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($_))}
 }
 
 function disclaimer {
