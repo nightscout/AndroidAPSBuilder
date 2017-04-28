f050245c59cb02debbfca17a3a3f14e2b8798974
LadyViktoria <leonieneon@gmail.com>
2017-04-28 08:38:56 +0200

    dotnet4.5

--- scripts/installPowershell5.ps1
+++ scripts/installPowershell5.ps1
@@ -3,6 +3,7 @@
 (get-host).ui.rawui.WindowTitle = "Install Powershell 5"
 
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco upgrade dotnet4.5 -y --force
 choco upgrade powershell -y --force
 write-host "Please Restart your Computer" -ForegroundColor Red
 Write-Host "Press Any Key To Continue... " 
